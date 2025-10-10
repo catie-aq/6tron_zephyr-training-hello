b022b222decab834e90460cc0795ab668b073c38
Bruno Perraudin <b.perraudin@catie.fr>
2025-10-10 15:30:14 +0200

    Initial import

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 CATIE
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello from %s!\n", CONFIG_BOARD_TARGET);
+
+  return 0;
+}
